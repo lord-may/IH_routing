562e5cec76f6cd73a0b6a5d3bae9dabfef21fc86
lord-may <cole@lgeo.co>
2025-12-17 16:59:34 -0700

    update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const basemapPortalID =  "6b801bba124b43838466c0f2113081a3"; ////Default basemap portal id ;  "12f2664ae36f452f95f42cdfecb39586"; //basemap portal id custom basemap.
-const routesPortalID = "a8fb797c78c44d3da37a0c728e66d604";
-const HubsandFacilitiesPortalID = "e9ef4b395acb4d2c92e6f7bf7c0a1c63";
+const basemapPortalID = "6b801bba124b43838466c0f2113081a3"; ////Default basemap portal id ;  "12f2664ae36f452f95f42cdfecb39586"; //basemap portal id custom basemap.
+const routesPortalID = "172ba72d27d94a69a7d7f9cffbeb6f7b"; // "a8fb797c78c44d3da37a0c728e66d604";
+const HubsandFacilitiesPortalID = "066d6df68b7e4fc5ae26a48c5e101c0e";  //"e9ef4b395acb4d2c92e6f7bf7c0a1c63"; 
 const barriersPortalID = "7029c8ab0c0a4761ad59cefd7192bfc6";
 const closuresPortalID = "b9c7dddda8cb4acf9997fcbb2c08dc96";
 const floodingPortalID = "221886c00ab6457ea555f766ba6c660a";
